32a877bd945e43f1f050cf45c0b3dc086bb7cbf9
Emmanuel <emmanuelochigbo60@gmail.com>
2025-04-27 08:58:10 -0400

    neuue

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-};
+const nextConfig: NextConfig = {};
 
 export default withNextIntl(nextConfig);
